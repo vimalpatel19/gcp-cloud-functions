26a67eaa558d0fd698bf958d560abc88418119d6
vimalp19 <vphb9@umsl.edu>
2020-04-10 15:06:04 -0500

    Began working on 1st API function.

---
+++ api/go.mod
@@ -0,0 +1,3 @@
+module github.com/vimalpatel19/gcp-cloud-functions/api
+
+go 1.14
---
+++ api/savepost.go
@@ -0,0 +1,11 @@
+package api
+
+import (
+	"net/http"
+)
+
+func SavePost(w http.ResponseWriter, r *http.Request) {
+	// ctx := context.Background()
+
+	w.WriteHeader(http.StatusOK)
+}
